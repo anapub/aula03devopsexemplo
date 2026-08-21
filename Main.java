853a0c86cc5a68f013c44c260635943669395d67
anapub <ana.uaquida@gmail.com>
2026-08-20 22:19:49 -0300

    nome2

--- Main.java
+++ Main.java
@@ -2,6 +2,7 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Olá, Devops");
         System.out.println("Pipeline executado com sucesso!");
-          System.out.println("Ana Paula");
+        System.out.println("Ana Paula");
+        System.out.println("Daniel");
     }
 }
